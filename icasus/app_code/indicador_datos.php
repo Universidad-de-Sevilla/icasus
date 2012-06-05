42646bdc72d30773b67ca626864fab7f8a7a801f
Juan Antonio Ruiz Rivas <juanan@us.es>
2012-06-05 13:30:00 +0200

    Ya funcionan listados y fichas de indicadores

--- icasus/app_code/indicador_datos.php
+++ icasus/app_code/indicador_datos.php
@@ -1,15 +1,12 @@
 <?php
 //---------------------------------------------------------------------------------------------------
-// Proyecto: Icasus (http://wiki.us.es/icasus/)
+// Proyecto: Icasus 
 // Archivo: indicador_datos.php
-// Tipo: controlador
-// Desarrolladores: Juanan Ruiz (juanan@us.es)
 //---------------------------------------------------------------------------------------------------
 // Descripcion: Muestra todos los parametros de un indicador y un listado de los valores introducidos
 //---------------------------------------------------------------------------------------------------
 
 global $smarty;
-global $basedatos;
 global $plantilla;
 
 $smarty->assign('_javascript' , array('ordenatabla','indicador_borrar','valor_borrar'));
@@ -17,28 +14,21 @@ $smarty->assign('_javascript' , array('ordenatabla','indicador_borrar','valor_bo
 // Comprueba que se ha especificado el indicador
 if (isset($_GET['id_indicador']))
 {
-	// Comprueba si viene aviso o error
-	if (isset($_GET['aviso']))
-	{
-		$smarty->assign('aviso', sanitize($_GET['aviso'],2));
-	}
-	if (isset($_GET['error']))
-	{
-		$smarty->assign('error', sanitize($_GET['error'],2));
-	}
-
 	// Prepara los datos a mostrar: indicador, valores, grafica
 	$id_indicador = sanitize($_GET['id_indicador'],16); 
-	$indicador = new indicador($basedatos);
-	$smarty->assign('indicador', $indicador->obtener_datos($id_indicador));
+	$indicador = new indicador();
+  $indicador->load_joined("id = $id_indicador");
+	$smarty->assign('indicador', $indicador);
 	// Obtiene los valores
-	$valores = $indicador->listar_valores($id_indicador);
+	//$valores = $indicador->listar_valores($id_indicador);
+  $valores = false;
 	$smarty->assign('valores', $valores );
 	// Datos de la entidad
-	$entidad = new entidad($basedatos);
-	$smarty->assign('entidad', $entidad->obtener_datos($indicador->datos['id_entidad']));
-	$smarty->assign('_nombre_pagina', $entidad->datos['nombre']); 
-	$smarty->assign('barra_indicadores',$entidad->barra_indicadores()); 
+	$entidad = new entidad();
+  $entidad->load("id = $indicador->id_entidad");
+	$smarty->assign('entidad', $entidad);
+	$smarty->assign('_nombre_pagina', $entidad->nombre); 
+	//$smarty->assign('barra_indicadores',$entidad->barra_indicadores()); 
 	// Dibuja la grafica
 	if ($valores) 
 	{
@@ -53,8 +43,6 @@ else
 {
 	// Faltan parametros avisa error
 	$error = "No se puede mostrar el indicador por falta de parámetros.";
-	$smarty->assign('error',$error);
-	$smarty->assign('_nombre_pagina' , 'Error'); 
-	$plantilla = "error.tpl";
+	header("index.php?page=entidad_listar");
 }
 ?>
--- icasus/app_code/indicador_listar.php
+++ icasus/app_code/indicador_listar.php
@@ -1,33 +1,33 @@
 <?php
 //---------------------------------------------------------------------------------------------------
-// Proyecto: Icasus (http://wiki.us.es/icasus/)
+// Proyecto: Icasus
 // Archivo: indicador_listar.php
-// Tipo: controlador
-// Desarrolladores: Juanan Ruiz (juanan@us.es), Jesús Martín (jjmc@us.es)
 //---------------------------------------------------------------------------------------------------
 // Descripcion: Listado de indicadores para una entidad determinada
 //---------------------------------------------------------------------------------------------------
 
 global $smarty;
-global $basedatos;
 global $plantilla;
 
 $smarty->assign('_javascript' , array('ordenatabla','indicador_borrar'));
 
-if ($_GET['id_entidad'] >= '0')
+if ($_GET['id_entidad'] > '0')
 {	
-	$id_entidad = sanitize($_GET["id_entidad"],16);
-	$entidad = new entidad($basedatos);
-	$smarty->assign('entidad', $entidad->obtener_datos($id_entidad));
-	$smarty->assign('indicadores',$entidad->listar_indicadores()); 
-	$smarty->assign('barra_indicadores',$entidad->barra_indicadores()); 
+	$id_entidad = sanitize($_GET["id_entidad"],INT);
+	$entidad = new entidad();
+  $entidad->load("id = $id_entidad");
+	$smarty->assign('entidad', $entidad);
+
+  $indicador = new indicador();
+  $indicadores = $indicador->Find_joined("id_entidad = $id_entidad");
+	$smarty->assign('indicadores',$indicadores); 
+	//$smarty->assign('barra_indicadores',$entidad->barra_indicadores()); 
+	$smarty->assign('_nombre_pagina' , $entidad->nombre);
 	$plantilla = 'indicador_listar.tpl';
-	$smarty->assign('_nombre_pagina' , $entidad->datos['nombre']);
 }
 else
 {
-	$smarty->assign('error','No ha especificado la entidad cuyos indicadores desea listar.');
-	$plantilla = 'error.tpl';
-	$smarty->assign('_nombre_pagina' , 'Error');
+	$error = "No ha especificado la entidad cuyos indicadores desea listar.";
+  header("location:index.php?page=error&error=$error");
 }
 ?>
