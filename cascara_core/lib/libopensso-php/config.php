a5f479507b425916994fd5df8a002ba3d0c08d7c
Juan Jesús Martín <jjmc@us.es>
2012-06-13 11:11:05 +0200

    Comentando la configuración del SSO y definiéndola en el app_config

--- cascara_core/lib/libopensso-php/config.php
+++ cascara_core/lib/libopensso-php/config.php
@@ -1,7 +1,10 @@
 <?php
 // Configuration package used
+
+// A partir de ahora la configuración del Opensso se define en el app_config de cada sitio
+
 //define('CONFIG_PACK', 'prod_1013');
-define('CONFIG_PACK', 'pre_11');
+//define('CONFIG_PACK', 'pre_11');
 
 
 // Package directory (do not change)
