e06c208b45648233fe1867f4adf0bc2b0b9ed184
Juan Jesús Martín Corredera <jjmc@MacBook-Pro-de-Juan-Jesus-Martin-Corredera.local>
2012-06-20 11:46:54 +0200

    Subiendo medicion crear y grabar

--- icasus/app_code/medicion_grabar.php
+++ icasus/app_code/medicion_grabar.php
@@ -9,13 +9,22 @@ global $smarty;
 global $usuario;
 global $plantilla;
 
-if (isset($_REQUEST["id_indicador"]) AND is_array($_REQUEST["id_unidades"]) AND isset($_REQUEST["periodo_inicio"]) AND isset($_REQUEST["periodo_fin"]))
+//datos periodo 
+$periodo_inicio = sanitize($_REQUEST["piYear"],INT)."-".sanitize($_REQUEST["piMonth"],INT)."-".sanitize($_REQUEST["piDay"],INT);
+$periodo_fin = sanitize($_REQUEST["pfYear"],INT)."-".sanitize($_REQUEST["pfMonth"],INT)."-".sanitize($_REQUEST["pfDay"],INT);
+$grabacion_inicio = sanitize($_REQUEST["giYear"],INT)."-".sanitize($_REQUEST["giMonth"],INT)."-".sanitize($_REQUEST["giDay"],INT);
+$grabacion_fin = sanitize($_REQUEST["gfYear"],INT)."-".sanitize($_REQUEST["gfMonth"],INT)."-".sanitize($_REQUEST["gfDay"],INT);
+if (isset($_REQUEST["id_indicador"]) AND is_array($_REQUEST["id_unidades"]) AND isset($periodo_inicio) AND isset($periodo_fin)AND isset($grabacion_inicio) AND isset($grabacion_fin))
 {
+	
   $medicion = new medicion();
-  $medicion->id_indicador = sanitize($_REQUEST["id_indicador"], INT);
-  $medicion->periodo_inicio = sanitize($_REQUEST["periodo_inicio"], SQL);
-  $medicion->periodo_fin = sanitize($_REQUEST["periodo_fin"], SQL);
-  $medicion->etiqueta = empty($_REQUEST["etiqueta"]) ? null : sanitize($_REQUEST["etiqueta"],SQL);
+ 
+	$medicion->id_indicador = sanitize($_REQUEST["id_indicador"], INT);
+  $medicion->periodo_inicio = $periodo_inicio;
+  $medicion->periodo_fin =  $periodo_fin;
+  $medicion->grabacion_inicio = $grabacion_inicio; 
+  $medicion->grabacion_fin = $grabacion_fin; 
+	$medicion->etiqueta = empty($_REQUEST["etiqueta"]) ? null : sanitize($_REQUEST["etiqueta"],SQL);
 
   if ($medicion->save())
   {
@@ -47,6 +56,7 @@ if (isset($_REQUEST["id_indicador"]) AND is_array($_REQUEST["id_unidades"]) AND 
 else
 {
   $error = "Faltan datos para procesar la petición de creación de medición.";
-  header("location:index.php?page=entidad_listar&error=$error");
+  header("location:index.php?page=indicador_mostrar&id_indicador=1977&id_entidad=14");
+  //header("location:index.php?page=entidad_listar&error=$error");
 }
 ?>
