c7474de4ca42a8bc8bfdb86c035ab11c75d91936
Juan Antonio Ruiz Rivas <juanan@us.es>
2012-10-08 11:23:50 +0200

    ya funciona el borrado de mediciones con comprobación de permisos y confirmación de borrado

--- icasus/app_code/medicion_editar.php
+++ icasus/app_code/medicion_editar.php
@@ -1,9 +1,9 @@
 <?php
 //---------------------------------------------------------------------------------------------------
 // Proyecto: Icasus 
-// Archivo: medicion_mostrar.php
+// Archivo: medicion_editar.php
 //---------------------------------------------------------------------------------------------------
-// Muestra los datos asociados a una medicion
+// Muestra los datos asociados a una medicion, si el usuario está autorizado le permite editarlo
 //---------------------------------------------------------------------------------------------------
 global $smarty;
 global $usuario;
@@ -38,6 +38,22 @@ if (isset($_REQUEST["id_medicion"]) AND isset($_REQUEST["tipo"]))
   $entidad = new entidad();
   $entidad->load("id = $indicador->id_entidad");
   $smarty->assign('entidad', $entidad);
+  
+  // Comprobamos si el usuario tiene autorización para borrar la medición
+  $usuario_entidad = new usuario_entidad();
+  if ($usuario_entidad->load("id_usuario=$usuario->id AND id_entidad=$entidad->id AND (id_rol = 1 OR id_rol =2)"))
+  {
+    $autorizado_borrar = true;
+  }
+  else if ($indicador->id_responsable == $usuario->id OR $indicador->id_responsable_medicion == $usuario->id)
+  {
+    $autorizado_borrar = true;
+  }
+  else
+  {
+    $autorizado_borrar = false;
+  }
+  $smarty->assign("autorizado_borrar", $autorizado_borrar);
   
   $smarty->assign("usuario", $usuario);
   $smarty->assign("_nombre_pagina", "$indicador->codigo - $indicador->nombre");
