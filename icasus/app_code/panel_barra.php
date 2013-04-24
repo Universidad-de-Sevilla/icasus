e191974c10dbe6ac274952a1da4f42c3aa9a76da
Jesús Martín Corredera <jjmc@us.es>
2013-04-24 13:52:02 +0200

    Paneles por id_mediciones y fechas

---
+++ icasus/app_code/panel_barra.php
@@ -0,0 +1,38 @@
+<?php
+$modulo = sanitize($_REQUEST["modulo"],SQL);
+$tipo = sanitize($_REQUEST["page"],SQL);
+
+$panel_tipo = new panel_tipo();
+$panel_tipo->load("clase_css = '$tipo'");
+$smarty->assign('panel' , $panel_tipo);
+
+if ($modulo == 'inicio')
+{
+	$id_entidad = sanitize($_REQUEST["id_entidad"],2);
+
+	$indicador = new indicador();
+	$indicadores = $indicador->find("id_entidad = $id_entidad");
+	$smarty->assign('indicadores' , $indicadores);
+
+	$smarty->assign('id_entidad' , $id_entidad);
+}
+if ($modulo == 'fecha_fin')
+{
+	$fecha_inicio = sanitize($_REQUEST["fecha_inicio"],2);
+	$smarty->assign('fecha_inicio' , $fecha_inicio);
+}
+if ($modulo == 'subunidades')
+{
+	$id_indicador = sanitize($_REQUEST["id_indicador"],2);
+	$nombre_indicador = sanitize($_REQUEST["nombre_indicador"],SQL);
+
+	$indicador_subunidad = new indicador_subunidad();
+	$indicador_subunidades = $indicador_subunidad->find_entidades("id_indicador = $id_indicador");
+	$smarty->assign("indicador_subunidades",$indicador_subunidades);
+	$smarty->assign("id_indicador",$id_indicador);
+	$smarty->assign("nombre_indicador",$nombre_indicador);
+}
+	$smarty->assign('modulo' , $modulo);
+	$smarty->assign('_nombre_pagina' , "Nuevo panel");
+	$plantilla = "$tipo.tpl";
+?>
