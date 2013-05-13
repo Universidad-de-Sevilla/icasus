0fb825c162148fa52e9c46745dcad75bb55e07ff
Jesús Martín Corredera <jjmc@us.es>
2013-05-13 13:54:35 +0200

    #1308 exportar datos rebiun

---
+++ icasus/app_code/informe_rebiun.php
@@ -0,0 +1,23 @@
+<?php
+//---------------------------------------------------------------------------------------------------
+// Proyecto: Icasus 
+// Archivo: consulta_avanzada.php
+//---------------------------------------------------------------------------------------------------
+// Consulta a los datos e indicadores, permitiendo hacer operaciones matemáticas sencillas entre ellos.
+//  - Genera una tabla de resultados y una gráfica
+//  - Permite elegir subunidades
+//  - Permite acotar los periodos de medición deseados
+//---------------------------------------------------------------------------------------------------
+$indicador = new indicador();
+$rebiun_detalle = $indicador->rebiun_2012_detalle();
+$smarty->assign("informe",$rebiun_detalle);
+
+$rebiun_suma = $indicador->rebiun_2012_suma();
+$smarty->assign("suma",$rebiun_suma);
+
+$rebiun_promedio = $indicador->rebiun_2012_promedio();
+$smarty->assign("promedio",$rebiun_promedio);
+
+$smarty->assign("_nombre_pagina", "Informe Rebiun 2012");
+$plantilla = "informe_rebiun.tpl";
+?>
