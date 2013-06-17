e7a0b3f52f46bddd51c73c4b3641da81fcab6ee6
Juan Antonio Ruiz Rivas <juanan@us.es>
2013-06-17 08:01:32 +0200

    empezando a codificar los volcados csv

--- icasus/app_code/volcado_grabar.php
+++ icasus/app_code/volcado_grabar.php
@@ -6,14 +6,41 @@
 // Descripcion: Graba el contenido de un  fichero (en principio CSV) en la base de datos
 //---------------------------------------------------------------------------------------------------
 
-$indicador = new indicador();
+if (isset($_FILES))
+{
+  
+  $row = 1;
+  $manejador = fopen($_FILES[0], "r");
+
+  if ($manejador !== FALSE) 
+  {
+    while (($data = fgetcsv($manejador, 1000, ",")) !== FALSE) 
+    {
+      $num = count($data);
+      echo "<p> $num fields in line $row: <br /></p>\n";
+      $row++;
+      for ($c=0; $c < $num; $c++) 
+      {
+        echo $data[$c] . "<br />\n";
+      }
+    }
+    fclose($manejador);
+  }
+  $indicador = new indicador();
+}
+else
+{
+  $error = 'No se ha especificado ningún archivo para subir';
+  header("Location: index.php?page=volcado_subir&error=$error");
+}
+
 
 //global $DB;
 //print_r($DB);
-$DB = $indicador->DB();
-echo $DB->Insert_ID();
+//$DB = $indicador->DB();
+//echo $DB->Insert_ID();
 //echo $DB->date();
 //echo $DB->Date();
-print_r($DB);
+//print_r($DB);
 ?>
 
---
+++ icasus/app_code/volcado_subir.tpl
@@ -0,0 +1,7 @@
+<form name="subida_ficheros" action="index.php?page=volcado_grabar">
+  <label for="fichero">
+    Elija un fichero para subir
+  </label>
+  <input name="fichero" type="file">
+  <input type="submit" value="Subir fichero e importar valores">
+</form>
