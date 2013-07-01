74aae19d5726f4080653ab61873bd1ab97a2ac00
Juan Antonio Ruiz Rivas <juanan@us.es>
2013-07-01 07:47:13 +0200

    volcado csv con seguridad y comprobacion indicadores

--- icasus/app_code/csv_grabar.php
+++ icasus/app_code/csv_grabar.php
@@ -5,62 +5,86 @@
 //---------------------------------------------------------------------------------------------------
 // Descripcion: Graba el contenido de un  fichero CSV en la base de datos
 //---------------------------------------------------------------------------------------------------
+global $smarty;
+global $plantilla;
 global $usuario;
+$usuario_entidad = new usuario_entidad();
 
 if (isset($_FILES, $_REQUEST['id_entidad']))
 {
 	$id_entidad = sanitize($_REQUEST['id_entidad'],INT);
+
   // Comprobamos que el usuario esté autorizado
-  $persona = new persona();
-  //TODO: permisos
-
-  $ficheros_procesados = 0;
-  $registros_grabados = 0;
-  $registros_totales = 0;
-
-  foreach ($_FILES["fichero_csv"]["error"] as $indice => $error)
+  if ($usuario_entidad->comprobar_responsable_entidad($usuario->id, $id_entidad))
   {
-    // Esto es una guarrería pero está sacada del manual oficial de php.net
-    // así que es una "guarrerida oficial"
-    if ($error == UPLOAD_ERR_OK) 
-    {
-      $tmp_name = $_FILES["fichero_csv"]["tmp_name"][$indice];
-      $name= $_FILES["fichero_csv"]["name"][$indice];
-      $manejador = fopen($tmp_name, "r");
+    $ficheros_procesados = 0;
+    $registros_grabados = 0; //los que realmente se graban 
+    $registros_ajenos = 0; // los que no pertenecen a la entidad actual
+    $registros_totales = 0;
 
-      if ($manejador !== FALSE) 
+    foreach ($_FILES["fichero_csv"]["error"] as $indice => $error)
+    {
+      // Esto es una guarrería pero está sacada del manual oficial de php.net
+      // así que es una "guarrerida oficial"
+      if ($error == UPLOAD_ERR_OK) 
       {
-        $ficheros_procesados ++;
-        while (($datos = fgetcsv($manejador, 1000, ",", "'")) !== FALSE) 
+        $tmp_name = $_FILES["fichero_csv"]["tmp_name"][$indice];
+        $name= $_FILES["fichero_csv"]["name"][$indice];
+        $manejador = fopen($tmp_name, "r");
+
+        if ($manejador !== FALSE) 
         {
-          $registros_totales ++;
-          $medicion = new medicion();
-          $medicion->id_indicador = $datos[0];
-          $medicion->periodo_inicio = $datos[1];
-          $medicion->periodo_fin = $datos[2];
-          $medicion->etiqueta = $datos[3];
-          if ($medicion->save())
+          $ficheros_procesados ++;
+          while (($datos = fgetcsv($manejador, 1000, ",", "'")) !== FALSE) 
           {
-            $valor = new valor();
-            $valor->id_medicion = $medicion->id;
-            $valor->id_usuario = $usuario->id;
-            $valor->id_entidad = $id_entidad;
-            $valor->fecha_recogida = date('Y-m-d h:m:i');
-            $valor->valor_parcial = $datos[4];
-            $valor->valor = $datos[4];
-            if ($valor->save())
+            $registros_totales ++;
+            $medicion = new medicion();
+            $medicion->id_indicador = $datos[0];
+            $medicion->periodo_inicio = $datos[1];
+            $medicion->periodo_fin = $datos[2];
+            $medicion->etiqueta = $datos[3];
+            // Comprobamos que el indicador pertenece a la entidad actual o a su madre
+            // Podría optimizarse comprobando si el indicador a cambiado respecto al anterior
+            $indicador = new indicador();
+            $entidad = new entidad();
+            $indicador->load("id = $medicion->id_indicador");
+            $entidad->load("id = $indicador->id_entidad");
+            if ($indicador->id_entidad == $id_entidad OR $entidad->id_madre == $id_entidad)
+            {         
+              if ($medicion->save())
+              {
+                $valor = new valor();
+                $valor->id_medicion = $medicion->id;
+                $valor->id_usuario = $usuario->id;
+                $valor->id_entidad = $id_entidad;
+                $valor->fecha_recogida = date('Y-m-d h:m:i');
+                $valor->valor_parcial = $datos[4];
+                $valor->valor = $datos[4];
+                if ($valor->save())
+                {
+                  $registros_grabados ++;
+                }
+              }
+            }
+            else
             {
-              $registros_grabados ++;
+              $registros_ajenos ++;
             }
           }
+          fclose($manejador);
         }
-        fclose($manejador);
+        // $indicador = new indicador();
       }
-      $indicador = new indicador();
     }
+    $aviso = "Se han procesado $ficheros_procesados ficheros y $registros_totales mediciones. Se han grabado $registros_grabados mediciones."; 
+    header("Location: index.php?page=csv_importar&id_entidad=$id_entidad&aviso=$aviso");
   }
-  $aviso = "Se han procesado $ficheros_procesados ficheros y $registros_totales mediciones. Se han grabado $registros_grabados mediciones."; 
-  header("Location: index.php?page=csv_importar&id_entidad=$id_entidad&aviso=$aviso");
+  else
+  {
+    $error = 'No tiene autorización para importar datos en esta unidad';
+    header('location:index.php?error=' . $error);
+  }
+
 }
 else
 {
--- icasus/app_code/csv_importar.php
+++ icasus/app_code/csv_importar.php
@@ -6,13 +6,25 @@
 // Descripcion: Permite subir un fichero para importar datos a la aplicación
 //---------------------------------------------------------------------------------------------------
 global $smarty;
+global $plantilla;
+global $usuario;
+$usuario_entidad = new usuario_entidad();
+
 
 if (isset($_REQUEST['id_entidad']))
 {
   $id_entidad = sanitize($_REQUEST['id_entidad'], INT);
-  $smarty->assign('id_entidad', $id_entidad);
-  $plantilla = 'csv_importar.tpl';
-  $smarty->assign('_nombre_pagina', 'Importar fichero CSV');
+  if ($usuario_entidad->comprobar_responsable_entidad($usuario->id, $id_entidad))
+  {
+    $smarty->assign('id_entidad', $id_entidad);
+    $plantilla = 'csv_importar.tpl';
+    $smarty->assign('_nombre_pagina', 'Importar fichero CSV');
+  }
+  else
+  {
+    $error = 'No tiene autorización para importar datos en esta unidad';
+    header('location:index.php?error=' . $error);
+  }
 }
 else
 {
