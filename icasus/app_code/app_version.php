f72492c62033bb1b046f4299b9a3f9780b36c364
Juan Antonio Ruiz Rivas <juanan@us.es>
2013-08-19 09:33:24 +0200

    modificando el orden de las versiones en app_version

--- icasus/app_code/app_version.php
+++ icasus/app_code/app_version.php
@@ -1,8 +1,9 @@
 <?php
-// 2.5.0 - Importación de datos desde ficheros CSV
-// 2.5.1 - Corregido error en tratamiento fichero CSV
-// 2.5.2 - Error: no grababa variable_control en ficha de procesos
-// 2.5.3 - Error: no grababa equipo_de_proceso en ficha de procesos
 define(IC_VERSION, "2.5.3");
 define(IC_FECHA_REVISION, "1/07/2013");
+
+// 2.5.3 - Error: no grababa equipo_de_proceso en ficha de procesos
+// 2.5.2 - Error: no grababa variable_control en ficha de procesos
+// 2.5.1 - Corregido error en tratamiento fichero CSV
+// 2.5.0 - Importación de datos desde ficheros CSV
 ?>
