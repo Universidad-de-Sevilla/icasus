fc18f7e5765f2ed8b7de3a436529cb773bad6218
Juan Antonio Ruiz Rivas <juanan@us.es>
2013-09-09 08:06:10 +0200

    Versión 2.5.4

--- icasus/app_code/app_version.php
+++ icasus/app_code/app_version.php
@@ -1,7 +1,8 @@
 <?php
-define(IC_VERSION, "2.5.3");
-define(IC_FECHA_REVISION, "1/07/2013");
+define(IC_VERSION, "2.5.4");
+define(IC_FECHA_REVISION, "9/09/2013");
 
+// 2.5.4 - Mejoras sustanciales en series temporales cuadros de mando
 // 2.5.3 - Error: no grababa equipo_de_proceso en ficha de procesos
 // 2.5.2 - Error: no grababa variable_control en ficha de procesos
 // 2.5.1 - Corregido error en tratamiento fichero CSV
