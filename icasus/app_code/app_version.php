55d7c13faa173feea4d60b9f9320e4d264e1ffde
Juan Antonio Ruiz Rivas <juanan@us.es>
2013-10-22 08:37:51 +0200

    Versión 2.5.6

--- icasus/app_code/app_version.php
+++ icasus/app_code/app_version.php
@@ -1,8 +1,9 @@
 <?php
-define(IC_VERSION, "2.5.5");
-define(IC_FECHA_REVISION, "17/10/2013");
+define(IC_VERSION, "2.5.6");
+define(IC_FECHA_REVISION, "22/10/2013");
 
-// 2.5.5 - Cambio de los paneles de barra para que muestren subunidades
+// 2.5.6 - Creación de paneles de barra mejorada
+// 2.5.5 - Cambio de los paneles de barra para que muestren subunidades, cambios en permisos sobre mediciones
 // 2.5.4 - Mejoras sustanciales en series temporales cuadros de mando
 // 2.5.3 - Error: no grababa equipo_de_proceso en ficha de procesos
 // 2.5.2 - Error: no grababa variable_control en ficha de procesos
