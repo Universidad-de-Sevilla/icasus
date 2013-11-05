5cd2219ca317bbe023ffc296e9cc9f9ad3841d6c
Juan Antonio Ruiz Rivas <juanan@us.es>
2013-11-05 17:11:36 +0100

    Ya funcionan bien los paneles de barra con mediciones puntuales

--- icasus/app_code/panel_barra.php
+++ icasus/app_code/panel_barra.php
@@ -10,6 +10,10 @@ $periodo = new periodo();
 $periodos = $periodo->Find("1 = 1");
 $smarty->assign("periodos", $periodos);
 
+$smarty->assign('modulo' , $modulo);
+$smarty->assign('_nombre_pagina' , "Nuevo panel");
+$plantilla = "$tipo.tpl";
+
 if ($modulo == 'inicio')
 {
 	$id_entidad = sanitize($_REQUEST["id_entidad"],2);
@@ -20,23 +24,21 @@ if ($modulo == 'inicio')
 
 	$smarty->assign('id_entidad' , $id_entidad);
 }
+
 if ($modulo == 'fecha_fin')
 {
 	$fecha_inicio = sanitize($_REQUEST["fecha_inicio"],2);
 	$smarty->assign('fecha_inicio' , $fecha_inicio);
 }
+
 if ($modulo == 'subunidades')
 {
 	$id_indicador = sanitize($_REQUEST["id_indicador"],2);
 	$nombre_indicador = sanitize($_REQUEST["nombre_indicador"],SQL);
-
 	$indicador_subunidad = new indicador_subunidad();
 	$indicador_subunidades = $indicador_subunidad->find_entidades("id_indicador = $id_indicador");
 	$smarty->assign("indicador_subunidades",$indicador_subunidades);
 	$smarty->assign("id_indicador",$id_indicador);
 	$smarty->assign("nombre_indicador",$nombre_indicador);
 }
-	$smarty->assign('modulo' , $modulo);
-	$smarty->assign('_nombre_pagina' , "Nuevo panel");
-	$plantilla = "$tipo.tpl";
 ?>
