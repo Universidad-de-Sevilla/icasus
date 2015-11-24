18c34f08a136a9de8f539138c72bb0dde8a86069
Joaquín Valonero Zaera <tecnibus1@us.es>
2015-11-24 21:31:12 +0100

    Avances en la migración a Bootstrap de la capa de presentación. Corregido fallo que mostraba warning en pantalla cuando el usuario no tenia indicadores o datos bajo su responsabilidad. Se han editado los controladores correspondientes.

--- icasus/app_code/indicadores.php
+++ icasus/app_code/indicadores.php
@@ -22,58 +22,60 @@ $smarty->assign('indicadores', $indicadores);
 $indicadores_propios = $indicador->Find_joined_ultima_medicion("(id_responsable = $usuario->id OR id_responsable_medicion = $usuario->id) AND id_proceso IS NOT NULL");
 $smarty->assign("indicadores_propios", $indicadores_propios);
 
-// Valores totales de las últimas mediciones
-$totales = array();
-$valor = new Valor();
-foreach ($indicadores_propios as $indicador)
+if ($indicadores_propios)
 {
-    $valores = $valor->find("id_medicion=" . $indicador->medicion->id);
-    $total = $logicaIndicador->calcular_total($indicador, $valores, $indicador->medicion->etiqueta);
-    $totales[$indicador->id] = $total;
-}
-$smarty->assign('totales', $totales);
+// Valores totales de las últimas mediciones
+    $totales = array();
+    $valor = new Valor();
+    foreach ($indicadores_propios as $indicador)
+    {
+        $valores = $valor->find("id_medicion=" . $indicador->medicion->id);
+        $total = $logicaIndicador->calcular_total($indicador, $valores, $indicador->medicion->etiqueta);
+        $totales[$indicador->id] = $total;
+    }
+    $smarty->assign('totales', $totales);
 
 //Control (Status) de valores limite y objetivo
-$valor_referencia = new Valor_referencia();
-$valor_referencia_medicion = new Valor_referencia_medicion();
-$medicion_lim = array();
-$medicion_obj = array();
-foreach ($indicadores_propios as $indicador)
-{
-    $valores_referencia = $valor_referencia->Find("id_indicador = $indicador->id");
-    if ($valores_referencia)
+    $valor_referencia = new Valor_referencia();
+    $valor_referencia_medicion = new Valor_referencia_medicion();
+    $medicion_lim = array();
+    $medicion_obj = array();
+    foreach ($indicadores_propios as $indicador)
     {
-        foreach ($valores_referencia as $valor_referencia)
+        $valores_referencia = $valor_referencia->Find("id_indicador = $indicador->id");
+        if ($valores_referencia)
         {
-            $existe = $valor_referencia_medicion->Load("id_valor_referencia=$valor_referencia->id AND id_medicion=" . $indicador->medicion->id);
-            if (!$existe)
+            foreach ($valores_referencia as $valor_referencia)
             {
-                $valor_referencia_medicion = new Valor_referencia_medicion();
-                $valor_referencia_medicion->id_valor_referencia = $valor_referencia->id;
-                $valor_referencia_medicion->id_medicion = $indicador->medicion->id;
-                $valor_referencia_medicion->save();
+                $existe = $valor_referencia_medicion->Load("id_valor_referencia=$valor_referencia->id AND id_medicion=" . $indicador->medicion->id);
+                if (!$existe)
+                {
+                    $valor_referencia_medicion = new Valor_referencia_medicion();
+                    $valor_referencia_medicion->id_valor_referencia = $valor_referencia->id;
+                    $valor_referencia_medicion->id_medicion = $indicador->medicion->id;
+                    $valor_referencia_medicion->save();
+                }
             }
-        }
-        $valores_referencia_medicion = $valor_referencia_medicion->Find_joined("id_medicion=" . $indicador->medicion->id);
-        foreach ($valores_referencia_medicion as $valor_referencia_medicion)
-        {
-            //Es la referencia Limite
-            if (strpos($valor_referencia_medicion->valor_referencia->etiqueta, 'mite') !== false)
+            $valores_referencia_medicion = $valor_referencia_medicion->Find_joined("id_medicion=" . $indicador->medicion->id);
+            foreach ($valores_referencia_medicion as $valor_referencia_medicion)
             {
-                $medicion_lim[$indicador->id] = $valor_referencia_medicion->valor;
-            }
-            //Es la referencia Objetivo
-            if (strpos($valor_referencia_medicion->valor_referencia->etiqueta, 'bjetivo') !== false)
-            {
-                $medicion_obj[$indicador->id] = $valor_referencia_medicion->valor;
+                //Es la referencia Limite
+                if (strpos($valor_referencia_medicion->valor_referencia->etiqueta, 'mite') !== false)
+                {
+                    $medicion_lim[$indicador->id] = $valor_referencia_medicion->valor;
+                }
+                //Es la referencia Objetivo
+                if (strpos($valor_referencia_medicion->valor_referencia->etiqueta, 'bjetivo') !== false)
+                {
+                    $medicion_obj[$indicador->id] = $valor_referencia_medicion->valor;
+                }
             }
         }
     }
+
+    $smarty->assign('medicion_obj', $medicion_obj);
+    $smarty->assign('medicion_lim', $medicion_lim);
 }
-
-$smarty->assign('medicion_obj', $medicion_obj);
-$smarty->assign('medicion_lim', $medicion_lim);
-
 $smarty->assign('_javascript', array('indicador_listar'));
 $smarty->assign('_nombre_pagina', FIELD_INDICS);
 $plantilla = 'indicadores.tpl';
--- icasus/app_code/inicio.php
+++ icasus/app_code/inicio.php
@@ -37,60 +37,73 @@ $cuadro = new Cuadro();
 $cuadros = $cuadro->Find("id_usuario = $usuario->id");
 $smarty->assign('cuadros_propios', $cuadros);
 
-$indicadores_datos = array_merge($indicadores, $datos);
-
-// Valores totales de las últimas mediciones
-$totales = array();
-$valor = new Valor();
-foreach ($indicadores_datos as $indicador)
+if (is_array($indicadores) && is_array($datos))
 {
-    $valores = $valor->find("id_medicion=" . $indicador->medicion->id);
-    $total = $logicaIndicador->calcular_total($indicador, $valores, $indicador->medicion->etiqueta);
-    $totales[$indicador->id] = $total;
+    $indicadores_datos = array_merge($indicadores, $datos);
 }
-$smarty->assign('totales', $totales);
+if (is_array($indicadores) && !is_array($datos))
+{
+    $indicadores_datos = $indicadores;
+}
+if (is_array($datos) && !is_array($indicadores))
+{
+    $indicadores_datos = $datos;
+}
+
+if (isset($indicadores_datos))
+{
+// Valores totales de las últimas mediciones
+    $totales = array();
+    $valor = new Valor();
+    foreach ($indicadores_datos as $indicador)
+    {
+        $valores = $valor->find("id_medicion=" . $indicador->medicion->id);
+        $total = $logicaIndicador->calcular_total($indicador, $valores, $indicador->medicion->etiqueta);
+        $totales[$indicador->id] = $total;
+    }
+    $smarty->assign('totales', $totales);
 
 //Control (Status) de valores limite y objetivo
-$valor_referencia = new Valor_referencia();
-$valor_referencia_medicion = new Valor_referencia_medicion();
-$medicion_lim = array();
-$medicion_obj = array();
-foreach ($indicadores_datos as $indicador)
-{
-    $valores_referencia = $valor_referencia->Find("id_indicador = $indicador->id");
-    if ($valores_referencia)
+    $valor_referencia = new Valor_referencia();
+    $valor_referencia_medicion = new Valor_referencia_medicion();
+    $medicion_lim = array();
+    $medicion_obj = array();
+    foreach ($indicadores_datos as $indicador)
     {
-        foreach ($valores_referencia as $valor_referencia)
+        $valores_referencia = $valor_referencia->Find("id_indicador = $indicador->id");
+        if ($valores_referencia)
         {
-            $existe = $valor_referencia_medicion->Load("id_valor_referencia=$valor_referencia->id AND id_medicion=" . $indicador->medicion->id);
-            if (!$existe)
+            foreach ($valores_referencia as $valor_referencia)
             {
-                $valor_referencia_medicion = new Valor_referencia_medicion();
-                $valor_referencia_medicion->id_valor_referencia = $valor_referencia->id;
-                $valor_referencia_medicion->id_medicion = $indicador->medicion->id;
-                $valor_referencia_medicion->save();
+                $existe = $valor_referencia_medicion->Load("id_valor_referencia=$valor_referencia->id AND id_medicion=" . $indicador->medicion->id);
+                if (!$existe)
+                {
+                    $valor_referencia_medicion = new Valor_referencia_medicion();
+                    $valor_referencia_medicion->id_valor_referencia = $valor_referencia->id;
+                    $valor_referencia_medicion->id_medicion = $indicador->medicion->id;
+                    $valor_referencia_medicion->save();
+                }
             }
-        }
-        $valores_referencia_medicion = $valor_referencia_medicion->Find_joined("id_medicion=" . $indicador->medicion->id);
-        foreach ($valores_referencia_medicion as $valor_referencia_medicion)
-        {
-            //Es la referencia Limite
-            if (strpos($valor_referencia_medicion->valor_referencia->etiqueta, 'mite') !== false)
+            $valores_referencia_medicion = $valor_referencia_medicion->Find_joined("id_medicion=" . $indicador->medicion->id);
+            foreach ($valores_referencia_medicion as $valor_referencia_medicion)
             {
-                $medicion_lim[$indicador->id] = $valor_referencia_medicion->valor;
-            }
-            //Es la referencia Objetivo
-            if (strpos($valor_referencia_medicion->valor_referencia->etiqueta, 'bjetivo') !== false)
-            {
-                $medicion_obj[$indicador->id] = $valor_referencia_medicion->valor;
+                //Es la referencia Limite
+                if (strpos($valor_referencia_medicion->valor_referencia->etiqueta, 'mite') !== false)
+                {
+                    $medicion_lim[$indicador->id] = $valor_referencia_medicion->valor;
+                }
+                //Es la referencia Objetivo
+                if (strpos($valor_referencia_medicion->valor_referencia->etiqueta, 'bjetivo') !== false)
+                {
+                    $medicion_obj[$indicador->id] = $valor_referencia_medicion->valor;
+                }
             }
         }
     }
+
+    $smarty->assign('medicion_obj', $medicion_obj);
+    $smarty->assign('medicion_lim', $medicion_lim);
 }
-
-$smarty->assign('medicion_obj', $medicion_obj);
-$smarty->assign('medicion_lim', $medicion_lim);
-
 $smarty->assign('_javascript', array('inicio'));
 $smarty->assign('_nombre_pagina', TXT_INICIO);
 $plantilla = 'inicio.tpl';
