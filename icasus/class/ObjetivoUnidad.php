c3f04060ebb2522607ca1b2dc04dd6d082d12874
Joaquín Valonero Zaera <tecnibus1@us.es>
2016-07-13 14:15:03 +0200

    Movido el método quitar_tildes a la clase Util para evitar repeticiones.

--- icasus/class/ObjetivoUnidad.php
+++ icasus/class/ObjetivoUnidad.php
@@ -34,17 +34,9 @@ class ObjetivoUnidad extends ADOdb_Active_Record
     //Ordena las subunidades de un Indicador/Dato por su etiqueta
     private function ordenarPorEtiqueta($a, $b)
     {
-        $cadena1 = $this->quitar_tildes($a->entidad->etiqueta);
-        $cadena2 = $this->quitar_tildes($b->entidad->etiqueta);
+        $cadena1 = Util::quitar_tildes($a->entidad->etiqueta);
+        $cadena2 = Util::quitar_tildes($b->entidad->etiqueta);
         return strcasecmp($cadena1, $cadena2);
-    }
-
-    private function quitar_tildes($cadena)
-    {
-        $no_permitidas = array("á", "é", "í", "ó", "ú", "Á", "É", "Í", "Ó", "Ú", "ñ", "À", "Ã", "Ì", "Ò", "Ù", "Ã™", "Ã ", "Ã¨", "Ã¬", "Ã²", "Ã¹", "ç", "Ç", "Ã¢", "ê", "Ã®", "Ã´", "Ã»", "Ã‚", "ÃŠ", "ÃŽ", "Ã”", "Ã›", "ü", "Ã¶", "Ã–", "Ã¯", "Ã¤", "«", "Ò", "Ã", "Ã„", "Ã‹");
-        $permitidas = array("a", "e", "i", "o", "u", "A", "E", "I", "O", "U", "n", "N", "A", "E", "I", "O", "U", "a", "e", "i", "o", "u", "c", "C", "a", "e", "i", "o", "u", "A", "E", "I", "O", "U", "u", "o", "O", "i", "a", "e", "U", "I", "A", "E");
-        $texto = str_replace($no_permitidas, $permitidas, $cadena);
-        return $texto;
     }
 
 }
