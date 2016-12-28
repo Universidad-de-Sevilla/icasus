8f019277bf72dcb7b75f85c8ce3033fa06209a9c
Joaquín Valonero Zaera <tecnibus1@us.es>
2016-12-28 13:00:24 +0100

    Eliminada constante innecesaria ERR_MED_TIPO_NO_BD.

--- icasus/app_code/lang_es.php
+++ icasus/app_code/lang_es.php
@@ -791,7 +791,6 @@ define('ERR_INDIC_MOSTRAR', 'No se puede mostrar el indicador, el identificador 
 
 //Mediciones
 define('ERR_MED_MOSTRAR', 'No se puede mostrar la medicion, el identificador no existe en la base de datos.');
-define('ERR_MED_TIPO_NO_BD', 'solicitado no existe en la base de datos');
 define('ERR_MED_NO_VAL', 'No existen valores pendientes ni recogidos para esta medición.');
 define('ERR_MED_GRABAR', 'Ha ocurrido un error al grabar la medición, inténtelo de nuevo o contacte con los administradores de icasus.');
 define('ERR_MED_BORRAR', 'No se puede borrar la medicion ya que influye en el cálculo de otros indicadores. Borre éstos primero');
