7f85ae0f21a51b3511f4cc476dad4e1616ba45d3
Joaquín Valonero Zaera <tecnibus1@us.es>
2017-03-17 12:47:59 +0100

    Revisados permisos de la ayuda, sólo los administradores de icasus pueden crear y editar páginas.

--- icasus/app_code/modules/ayuda/pagina_editar.php
+++ icasus/app_code/modules/ayuda/pagina_editar.php
@@ -10,33 +10,52 @@
 //---------------------------------------------------------------------------------------------------
 global $smarty;
 global $plantilla;
+global $usuario;
 
-if (filter_has_var(INPUT_GET, 'id_pagina'))
+// Sólo administradores de Icasus pueden editar páginas
+$admin = false;
+foreach ($usuario->entidades as $entidad)
 {
-    $pagina = new Pagina();
-    $id_pagina = filter_input(INPUT_GET, 'id_pagina', FILTER_SANITIZE_NUMBER_INT);
-    $pagina->load("id=$id_pagina");
-    $smarty->assign('pagina', $pagina);
-    $smarty->assign('_nombre_pagina', TXT_PAG_EDIT . ': ' . $pagina->titulo);
-    if (filter_has_var(INPUT_POST, 'titulo') && filter_has_var(INPUT_POST, 'alias') && filter_has_var(INPUT_POST, 'contenido'))
+    if ($entidad->id_entidad == 1 && ($entidad->id_rol == 1 || $entidad->id_rol == 2))
     {
-        $pagina->titulo = filter_input(INPUT_POST, 'titulo');
-        $pagina->alias = $pagina->alieniza(filter_input(INPUT_POST, 'alias'));
-        $pagina->contenido = filter_input(INPUT_POST, 'contenido');
-        $pagina->modified = time();
-        $pagina->save();
-        $exito = MSG_PAG_EDIT_OK;
-        header("Location: index.php?page=pagina_mostrar&alias=$pagina->alias&exito=$exito");
+        $admin = true;
+    }
+}
+
+if ($admin)
+{
+    if (filter_has_var(INPUT_GET, 'id_pagina'))
+    {
+        $pagina = new Pagina();
+        $id_pagina = filter_input(INPUT_GET, 'id_pagina', FILTER_SANITIZE_NUMBER_INT);
+        $pagina->load("id=$id_pagina");
+        $smarty->assign('pagina', $pagina);
+        $smarty->assign('_nombre_pagina', TXT_PAG_EDIT . ': ' . $pagina->titulo);
+        if (filter_has_var(INPUT_POST, 'titulo') && filter_has_var(INPUT_POST, 'alias') && filter_has_var(INPUT_POST, 'contenido'))
+        {
+            $pagina->titulo = filter_input(INPUT_POST, 'titulo');
+            $pagina->alias = $pagina->alieniza(filter_input(INPUT_POST, 'alias'));
+            $pagina->contenido = filter_input(INPUT_POST, 'contenido');
+            $pagina->modified = time();
+            $pagina->save();
+            $exito = MSG_PAG_EDIT_OK;
+            header("Location: index.php?page=pagina_mostrar&alias=$pagina->alias&exito=$exito");
+        }
+        else
+        {
+            $pagina->load("id=$id_pagina");
+            $plantilla = 'ayuda/pagina_editar.tpl';
+        }
     }
     else
     {
-        $pagina->load("id=$id_pagina");
-        $plantilla = 'ayuda/pagina_editar.tpl';
+        // Si no vienen datos suficientes en el post ni el get lanzamos error
+        $error = ERR_PARAM;
+        header("location:index.php?page=error&error=$error");
     }
 }
 else
 {
-    // Si no vienen datos suficientes en el post ni el get lanzamos error
-    $error = ERR_PARAM;
+    $error = ERR_PERMISOS;
     header("location:index.php?page=error&error=$error");
 }
