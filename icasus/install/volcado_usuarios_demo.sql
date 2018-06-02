5cb26b566f11d3a36190550baaeb8878a17db83a
Juanan Ruiz <juanan@us.es>
2018-06-02 19:48:09 +0200

    Faltaban los usuarios demo y había un error en el script de ajustes

---
+++ icasus/install/volcado_usuarios_demo.sql
@@ -0,0 +1,4 @@
+INSERT INTO `icasus_usuario` (`id`, `login`, `clave`, `nombre`, `apellidos`, `nif`, `correo`, `telefono`, `mis_notas`, `unidad_hominis`, `puesto`)
+VALUES
+  (1, 'admin', 'admin', 'Admin', 'Icasus', '11111111H', 'icasus@us.es', NULL, NULL, NULL, NULL),
+  (2, 'demo', 'demo', 'Demo', 'Icasus', '12345678A', 'demo@us.es', NULL, NULL, NULL, NULL);
