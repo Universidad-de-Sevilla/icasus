b3b376e063273aaa8cd6aa1ab54334627f0d496b
RAMON MARIA GOMEZ LABRADOR <ramongomez@us.es>
2021-02-22 11:38:56 +0100

    Tomar correctamente los datos del SSO.

--- icasus/app_code/control/login/login_cas.php
+++ icasus/app_code/control/login/login_cas.php
@@ -1,5 +1,4 @@
 <?php
-
 require_once __DIR__ . '/../../../../cascara_core/phpCAS-1.3.8/CAS.php';
 
 // Instancia el cliente
@@ -15,21 +14,32 @@ $smarty->assign("_nombre_pagina", TXT_BIENVENIDO);
 if (filter_has_var(INPUT_GET, 'autenticar')) {
     $url = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
     $usuario_sso_relacion = phpCAS::getAttribute('usesrelacion');
-    $usuario_sso_login = phpCAS::getAttribute('uid;');
+    $usuario_sso_afiliacion = phpCAS::getAttribute('edupersonaffiliation');
+    $usuario_sso_login = phpCAS::getAttribute('uid');
     $usuario_sso_nombre = phpCAS::getAttribute('givenname');
     $usuario_sso_apellidos = phpCAS::getAttribute('sn');;
     $usuario_sso_nif = phpCAS::getAttribute('irispersonaluniqueid');
     $usuario_sso_correo = phpCAS::getAttribute('irismailmainaddress');
+    /*
+    print_r("relacion:".$usuario_sso_relacion);
+    print_r("afiliacion:".$usuario_sso_afiliacion);
+    print_r("nif:".$usuario_sso_nif);
+    print_r("login:".$usuario_sso_login);
+    print_r("nombre:".$usuario_sso_nombre);
+    print_r("apellidos:".$usuario_sso_apellidos);
+    print_r("correo:".$usuario_sso_correo);
+    exit();
+    */
     $acceso_autorizado = false;
+    $perfiles_validos = ['PAS', 'PDI', 'MISCELANEA', 'PDIEXTERNO'];
     if (is_array($usuario_sso_relacion)) {
         foreach ($usuario_sso_relacion as $perfil) {
-            if ($perfil == 'PAS' or $perfil == 'PDI' or $perfil == 'MISCELANEA' or $perfil == 'PDIEXTERNO') {
+            if (in_array($perfil, $perfiles_validos)) {
                 $acceso_autorizado = true;
             }
         }
     } else {
-        if ($usuario_sso_relacion == 'PAS' or $usuario_sso_relacion == 'PDI' or $usuario_sso_relacion == 'MISCELANEA'
-            or $usuario_sso_relacion == 'PDIEXTERNO') {
+        if (in_array($usuario_sso_relacion, $perfiles_validos)) {
             $acceso_autorizado = true;
         }
     }
@@ -38,11 +48,9 @@ if (filter_has_var(INPUT_GET, 'autenticar')) {
     if ($acceso_autorizado) {
         $usuario = new Usuario();
 
-        if ($usuario->load_joined("login = '" . $usuario_sso_login . "'")) {
+        if ($usuario->load_joined("login = '$usuario_sso_login'")) {
             // Si el usuario existe en icasus cargamos sus datos
             $_SESSION['usuario'] = $usuario;
-            //$log = new $log;
-            //$log->add('login',0,$usuario->id);
             // Si el usuario tiene unidades asignadas
             if ($usuario->entidades) {
                 foreach ($usuario->entidades as $entidad) {
@@ -57,9 +65,9 @@ if (filter_has_var(INPUT_GET, 'autenticar')) {
         } else {
             // Si el usuario no existe lo damos de alta con los datos de ldap
             $usuario->login = $usuario_sso_login;
-            $usuario->nombre = $usuario_sso_apellidos;
-            $usuario->nif = $usuario_sso_nif;
+            $usuario->nombre = $usuario_sso_nombre;
             $usuario->apellidos = $usuario_sso_apellidos;
+            $usuario->nif = $usuario_sso_nif;
             $usuario->correo = $usuario_sso_correo;
             if ($usuario->save()) {
                 $_SESSION['usuario'] = $usuario;
